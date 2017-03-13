d4fa4e9c870c00140294a344435fff48bfbeeb10
Aya <ayabonga@projectcodex.co>
2017-03-13 16:32:31 +0200

    removed feature service

--- routes/songs.js
+++ routes/songs.js
@@ -2,14 +2,14 @@ module.exports = function(){
 
   this.getFeatured = function(req, res, next){
         req.services(function(err,services){
-              var featureService = services.featureDataServ;
-              featureService.getSongs(function(err, results) {
+              var songService = services.songDataServ;
+              songService.getFeatured(function(err, results) {
                   console.log(results)
                   res.render('home',{songs:results})
               })
         });
   }
-  this.getAllSongs = function(req, res, next){
+  this.getAll = function(req, res, next){
         req.services(function(err,services){
               var songService = services.songDataServ;
               songService.getSongs(function(err, results) {
