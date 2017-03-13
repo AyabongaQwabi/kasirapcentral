174d2fc51d993f6aecafa7ed45283fe2ad203c28
Aya <ayabonga@projectcodex.co>
2017-03-13 16:30:15 +0200

    Created feature service to feth songs and videos

---
+++ dataServices/featureDataservice.js
@@ -0,0 +1,15 @@
+module.exports = function(connection){
+
+
+
+  var executeQuery = function(query, data, cb){
+      connection.query(query, data, cb);
+  };
+
+  this.getSongs = function(cb){
+      executeQuery('select featured.song_id , song.name as name ,user.id as user_id,user.name as artist ,image_art.src as art from featured,song,user, image_art where featured.song_id =song.id and song.user_id = user.id and image_art.song_id = song.id',cb);
+  }
+  this.getVideos = function(cb){
+      executeQuery('select featured.song_id , song.name as name ,user.id as user_id,user.name as artist ,image_art.src as art from featured,song,user, image_art where featured.song_id =song.id and song.user_id = user.id and image_art.song_id = song.id',cb);
+  }
+}
