82eabc601b95025ea63e0a941b26425b8b5fe3e2
Aya <ayabonga@projectcodex.co>
2017-03-13 16:32:57 +0200

    swapped feature service for video service

--- routes/videos.js
+++ routes/videos.js
@@ -2,19 +2,19 @@ module.exports = function(){
 
   this.getFeatured = function(req, res, next){
         req.services(function(err,services){
-              var featureService = services.featureDataServ;
-              featureService.getSongs(function(err, results) {
+              var videoService = services.videoDataServ;
+              videoService.getFeatured(function(err, results) {
                   console.log(results)
-                  res.render('home',{songs:results})
+                  res.render('home',{videos:results})
               })
         });
   }
-  this.getAllSongs = function(req, res, next){
+  this.getAll = function(req, res, next){
         req.services(function(err,services){
-              var songService = services.songDataServ;
-              songService.getSongs(function(err, results) {
+              var videoService = services.videoDataServ;
+              videoService.getVideos(function(err, results) {
                   console.log(results)
-                  res.render('songlist',{songs:results})
+                  res.render('videolist',{videos:results})
               })
         });
   }
