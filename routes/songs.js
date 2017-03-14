a48eb1af4bf18150940a073f6275901639a2a2d5
Ayabonga <aya@westerncapelabs.com>
2017-03-15 01:48:01 +0200

    Added update flame,play and download database refactor methods

--- routes/songs.js
+++ routes/songs.js
@@ -4,7 +4,7 @@ module.exports = function(){
         req.services(function(err,services){
               var songService = services.songDataServ;
               songService.getFeatured(function(err, results) {
-                  console.log(results)
+                  //console.log(results)
                   res.render('home',{songs:results})
               })
         });
@@ -13,10 +13,60 @@ module.exports = function(){
         req.services(function(err,services){
               var songService = services.songDataServ;
               songService.getSongs(function(err, results) {
-                  console.log(results)
+                  //console.log(results)
                   res.render('songlist',{songs:results})
               })
         });
   }
+  this.updateFlameCount = function(req, res, next){
+        console.log('User updating flame')
+        req.services(function(err,services){
+              var songService = services.songDataServ;
+              songService.getSongs(function(err, results) {
+                  results.forEach(function(result){
+                    if(result.id == req.body.song_id){
+                      var flames = (result.flame_count+=1);
+                      var updateData =[{flame_count:flames},{id:result.id}]
+                      songService.updateSong(updateData)
+                      res.send({flames:flames})
+                    }
+                  })
 
+              })
+        });
+  }
+  this.updatePlayCount =function(req, res, next){
+    console.log('User updating flame')
+        req.services(function(err,services){
+              var songService = services.songDataServ;
+              songService.getSongs(function(err, results) {
+                  results.forEach(function(result){
+
+                    if(result.id == req.body.song_id){
+                      var plays = (result.play_count+=1);
+                      var updateData =[{play_count:plays},{id:result.id}]
+                      songService.updateSong(updateData)
+                      res.send({plays:plays})
+                    }
+                  })
+
+              })
+        });
+  }
+  this.updateDownloadCount =function(req, res, next){
+        req.services(function(err,services){
+              var songService = services.songDataServ;
+              songService.getSongs(function(err, results) {
+                  results.forEach(function(result){
+                    if(result.id == req.body.song_id){
+                      var downloads = (result.download_count+=1);
+                      var updateData =[{download_count:downloads},{id:result.id}]
+                      songService.updateSong(updateData)
+                      res.send({downloads:downloads})
+                    }
+                  })
+
+              })
+        });
+  }
 }
