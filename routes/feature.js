8d361da47ad9bf6a79a4c8ff5b3e5e77fb059266
Ayabonga <aya@westerncapelabs.com>
2017-03-14 02:35:10 +0200

    Added get all featured videos

--- routes/feature.js
+++ routes/feature.js
@@ -1,13 +1,27 @@
 module.exports = function(){
+  var getId = function(url) {
+        var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+        var match = url.match(regExp);
 
+        if (match && match[2].length == 11) {
+            return match[2];
+        } else {
+            return 'error';
+        }
+    }
   this.getFeatured = function(req, res, next){
         req.services(function(err,services){
               var songService = services.songDataServ;
               var videoService = services.videoDataServ;
               songService.getFeatured(function(err, results) {
+                 var songs = results;
                 videoService.getFeatured(function(err, results) {
+                    results.forEach(function(result){
+                      result.url=getId(result.url)
+                    })
                     console.log(results)
-                    res.render('home',{songs:results})
+                    console.log('rendering songs')
+                    res.render('home',{songs:songs,videos:results})
                 })
             })
         });
