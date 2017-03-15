fb0f0ad395bb863ac89d0c77aa6289cfa5ceb74a
Aya <ayabonga@projectcodex.co>
2017-03-15 17:28:00 +0200

    modified featured

--- routes/feature.js
+++ routes/feature.js
@@ -16,7 +16,11 @@ module.exports = function(){
               var eventService = services.eventDataServ;
               songService.getFeatured(function(err, results) {
                  var songs = results;
-                 //console.log(results);
+                 songs.forEach(function(song){
+                   song.url = song.url.toString();
+                   console.log(song.url);
+                 })
+                 console.log(results[5].url.toString());
                 videoService.getFeatured(function(err, results) {
                     results.forEach(function(result){
                       result.url=getId(result.url)
