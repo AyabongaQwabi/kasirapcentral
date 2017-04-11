d28b16b6537b2e0af061add16c5c8bb226aa4381
Ayabonga <aya@westerncapelabs.com>
2017-04-11 16:57:25 +0200

    Added group module

---
+++ routes/groups.js
@@ -0,0 +1,71 @@
+module.exports = function(){
+
+  this.add = function(req, res, next){
+        req.services(function(err,services){
+              var data = req.body
+              console.log(data)
+              var groupService = services.groupDataServ;
+              groupService.add(data,function(err, results) {
+                  if(err){
+                    console.log(err)
+                  }
+                  groupService.fetch(function(err,results){
+                    var filteredResults=[]
+                    results.forEach(function(result){
+                       var r = result.link.split('.')
+                       var groupLink=false
+                       console.log(r)
+                       r.forEach(function(rlet){
+                         rlet.toLowerCase()
+                       })
+                       if(r[0].split('/').indexOf('chat') ==1){
+                         console.log('has chat')
+                         groupLink = true
+                       }
+                       if(r.indexOf('whatsapp') == 1){
+                         console.log('has whatsapp')
+                         groupLink = true
+                       }
+
+                       if(groupLink){
+                         filteredResults.push(result)
+                       }
+
+                    })
+                    res.render('groups',{groups:filteredResults})
+                  })
+              })
+        })
+  }
+  this.getGroups = function(req,res,next){
+    req.services(function(err,services){
+        var groupService = services.groupDataServ;
+        groupService.fetch(function(err,results){
+          var filteredResults=[]
+          results.forEach(function(result){
+             var r = result.link.split('.')
+             var groupLink=false
+             console.log(r)
+             r.forEach(function(rlet){
+               rlet.toLowerCase()
+             })
+             if(r[0].split('/').indexOf('chat') ==1){
+               console.log('has chat')
+               groupLink = true
+             }
+             if(r.indexOf('whatsapp') == 1){
+               console.log('has whatsapp')
+               groupLink = true
+             }
+
+             if(groupLink){
+               filteredResults.push(result)
+             }
+
+          })
+          res.render('groups',{groups:filteredResults})
+        })
+    })
+  }
+
+}
