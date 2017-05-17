42a3f807d0b40d39642f1c977aae35d96258867e
ayabongaqwabi <ayabonga@projectcodex.co>
2017-05-17 20:04:48 +0200

    optimised home page

--- routes/feature.js
+++ routes/feature.js
@@ -27,7 +27,7 @@ module.exports = function(){
                     })
                     var videos = results;
                     eventService.getEvents(function(err, results) {
-                      res.render('home',{songs:songs,videos:videos,events:results,layout:"index.video.handlebars"})
+                      res.render('home',{songs:songs,videos:videos,events:results})
                     })
 
                 })
